db9e44124d51488d39c98eb70d953ab9b1272e64
dina <dina.elcg@gmail.com>
2018-12-07 19:55:29 -0800

    firebase pushnotification

---
+++ app/src/main/java/com/gmail/dina_elsaftawy/merchantorder/firebase/notification/MyFirebaseInstanceIDService.java
@@ -0,0 +1,18 @@
+package com.gmail.dina_elsaftawy.merchantorder.firebase.notification;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("Token", "Refreshed token: " + refreshedToken);
+
+    }
+}
+
